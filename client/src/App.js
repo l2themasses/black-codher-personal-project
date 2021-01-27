7b790ce4eb1c2488dccff8f7cfb5428855b604d2
l <codn3@outlook.com>
2021-01-27 19:18:30 +0000

    forth commit , App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Item from './components/Item';
 import ItemList from './components/ItemList';
 import data from './models/items.json';
 
@@ -7,13 +6,6 @@ const App = (props) => {
 
      const [items] = useState(data);
 
-     const App = (props) => {
-        const [items] = useState(data);
-        
-       
-        return <ItemList items={items} />
-       }
-       
      if (items.length === 0 ) {
          return 'No items found' ;
      }
